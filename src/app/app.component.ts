8963c638c28a689184cde5083bac8c5bb3e30ece
farex111 <giorgi.paradashvili13@gmail.com>
2021-08-03 19:02:48 +0400

    last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  collapsed : any = document.querySelector('.navbar-toggler');
+  show : any = document.querySelector('.navbar-collapse')
   title = 'final-project';
+  constructor(private router: Router) { }
+  menu(){
+    this.collapsed.classList.add("Collapsed");
+    this.show.classList.add("Show");
+    console.log(this.collapsed)
+  }
+
+  ngOnInit() {
+      this.router.events.subscribe((evt) => {
+          if (!(evt instanceof NavigationEnd)) {
+              return;
+          }
+          window.scrollTo(0, 0)
+      });
+  }
 }
